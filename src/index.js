c89519080c72b70af7aa53a3b74cd3ad1150f852
Aliaksei Stelmakh <alexeistel@gmail.com>
2020-03-07 21:19:48 +0300

    feat: add solution brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+ let arr = [];
+ if (str == undefined) {
+   return;
+ } 
+ for (let i = 0; i < str.length; i++){
+   for (let j = 0; j < bracketsConfig.length; j++) {
+     if (str[i] === bracketsConfig[j][0]) {
+       arr.push(str[i]);
+     }
+     else if (str[i] === bracketsConfig[j][1]) {
+        for (let k = 0; k < i; k++) {
+           if (arr[k] === bracketsConfig[j][0]) {
+            arr.push(str[i]);
+           }
+        }
+     }
+        
+        }
+    }
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === arr[i]) {
+            return true;
+        }
+    }
+} 
