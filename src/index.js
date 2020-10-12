d86c652a65a78620dc42310d5d44aece6959faf1
Aliaksei Stelmakh <alexeistel@gmail.com>
2020-10-12 15:11:52 +0300

    fix: change task

--- src/index.js
+++ src/index.js
@@ -1,26 +1,50 @@
 module.exports = function check(str, bracketsConfig) {
- let arr = [];
- if (str == undefined) {
-   return;
- } 
- for (let i = 0; i < str.length; i++){
-   for (let j = 0; j < bracketsConfig.length; j++) {
-     if (str[i] === bracketsConfig[j][0]) {
-       arr.push(str[i]);
-     }
-     else if (str[i] === bracketsConfig[j][1]) {
-        for (let k = 0; k < i; k++) {
-           if (arr[k] === bracketsConfig[j][0]) {
-            arr.push(str[i]);
-           }
+    let stackBrackets = [];
+
+     const isOpenBrackets = (currentBrackets) => {
+        for (let i = 0; i < bracketsConfig.length; i++) {
+
+            const [openBrackets, closeBrackets] = bracketsConfig[i];
+
+            if (currentBrackets === openBrackets) {
+                if (openBrackets === closeBrackets) return -1;
+
+                return closeBrackets;
+            }
         }
-     }
-        
+        return false;
+    };
+
+    const getLastElement = (array) => {
+        if (Array.isArray(array))
+            return array[array.length - 1];
+        else
+            throw new Error('error');
+    };
+
+    if ((str.length % 2)) return false;
+
+    str.split('').forEach(item => {
+
+        const closeBracket = isOpenBrackets(item);
+
+        if (closeBracket === false) {
+
+            return item === getLastElement(stackBrackets)
+                ? stackBrackets.pop()
+                : false;
+
+        } else if (closeBracket === -1) {
+
+            item === getLastElement(stackBrackets)
+                ? stackBrackets.pop()
+                : stackBrackets.push(item);
+
+        } else {
+            stackBrackets.push(closeBracket);
         }
-    }
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === arr[i]) {
-            return true;
-        }
-    }
-} 
+    });
+
+    return !stackBrackets.length;
+};
+
